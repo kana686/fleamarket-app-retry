b15afdeee9bb9a71bd11ea245f80ae2221fc901b
kana686 <inui.k.w6@gmail.com>
2026-06-05 15:58:32 +0900

    feat:会員登録画面を作成 #5

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="register-container">
+    <h1>会員登録</h1>
+
+    <form action="/register" method="POST">
+        @csrf
+
+        {{-- コンポーネントを呼び出し --}}
+        <x-input-field name="name" label="ユーザー名" type="text" />
+        <x-input-field name="email" label="メールアドレス" type="email" />
+        <x-input-field name="password" label="パスワード" type="password" />
+        <x-input-field name="password_confirmation" label="確認用パスワード" type="password" />
+
+        <button type="submit">登録する</button>
+    </form>
+
+    <a href="/login">ログインはこちら</a>
+</div>
+@endsection
