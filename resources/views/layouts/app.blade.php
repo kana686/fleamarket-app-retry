90941a1f4b03e3b2d3bc4bb276fcc1f7f5af726d
kana686 <inui.k.w6@gmail.com>
2026-06-05 15:17:57 +0900

    feat:共通レイアウトを作成 #5

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECHフリマ</title>
+</head>
+<body>
+
+    <header>
+        <div class="header-logo">
+            <img src="{{ asset('images/coachtech-logo-header.png') }}" alt="COACHTECH">
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+</html>
