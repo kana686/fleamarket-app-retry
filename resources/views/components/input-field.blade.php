5ab7d05d34c178b5d5fad74b26bde57b99df437b
kana686 <inui.k.w6@gmail.com>
2026-06-05 15:41:51 +0900

    feat:入力フォームのコンポーネントを作成 #5

---
+++ resources/views/components/input-field.blade.php
@@ -0,0 +1,14 @@
+<div class="form-group">
+    @if(isset($label))
+        <label>{{ $label }}</label>
+    @endif
+
+    <input type="{{ $type ?? 'text' }}" 
+           name="{{ $name }}" 
+           class="form-control @error($name) is-invalid @enderror" 
+           value="{{ old($name) }}">
+
+    @error($name)
+        <span class="error-message">{{ $message }}</span>
+    @enderror
+</div>
