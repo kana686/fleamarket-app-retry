b40f9ad2e496fa095c6deac6a215ac5e2abf9020
kana686 <inui.k.w6@gmail.com>
2026-06-06 16:13:04 +0900

    refactor:継承方式からコンポーネント方式への修正とページタイトルが自動で変わるように修正 #5

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,22 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="title">会員登録</x-slot>
 
-@section('content')
-<div class="register-container">
-    <h1>会員登録</h1>
+    <div class="register-container">
+        <h1>会員登録</h1>
 
-    <form action="/register" method="POST">
-        @csrf
+        <form action="/register" method="POST">
+            @csrf
 
-        <x-input-field name="name" label="ユーザー名" type="text" />
-        <x-input-field name="email" label="メールアドレス" type="email" />
-        <x-input-field name="password" label="パスワード" type="password" />
-        <x-input-field name="password_confirmation" label="確認用パスワード" type="password" />
+            <x-input-field name="name" label="ユーザー名" type="text" />
+            <x-input-field name="email" label="メールアドレス" type="email" />
+            <x-input-field name="password" label="パスワード" type="password" />
+            <x-input-field name="password_confirmation" label="確認用パスワード" type="password" />
 
-        <x-primary-button>登録する</x-primary-button>
-    </form>
+            <x-primary-button>登録する</x-primary-button>
+        </form>
 
-    <div class="link-wrapper">
-        <a href="/login">ログインはこちら</a>
+        <div class="link-wrapper">
+            <a href="/login">ログインはこちら</a>
+        </div>
     </div>
-</div>
-@endsection
+</x-app-layout>
