1418c7842928f87dc38bad31f20d927aa887f964
kana686 <inui.k.w6@gmail.com>
2026-06-07 16:37:50 +0900

    feat:ログイン画面のviewを作成 #9

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="title">ログイン</x-slot>
+
+    <div class="login-container">
+        <h1>ログイン</h1>
+
+        <form action="/login" method="POST" novalidate>
+            @csrf
+
+            <x-input-field name="email" label="メールアドレス" type="email" />
+            <x-input-field name="password" label="パスワード" type="password" />
+
+            <x-primary-button>ログインする</x-primary-button>
+        </form>
+
+        <div class="link-wrapper">
+            <a href="/register">会員登録はこちら</a>
+        </div>
+    </div>
+</x-app-layout>
