7bba8e57e10b851262ca17ce876eab3344c89715
kana686 <inui.k.w6@gmail.com>
2026-06-07 17:39:56 +0900

    feat:ログイン画面が表示されるようにルートを再設定 #9

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/home', function () {
     return '登録成功！';
 });
 
-Route::get('/login', function () {
-    return 'ログイン画面へようこそ！';
-});
-
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
