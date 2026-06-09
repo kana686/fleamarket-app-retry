ee85efc24f2fdfd7cdeb931c795d811ccd3ea1f4
kana686 <inui.k.w6@gmail.com>
2026-06-09 16:16:14 +0900

    fix:ログイン成功後のページへ遷移できるように修正 #10

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Pipeline\Pipeline;
 use Laravel\Fortify\Actions\AttemptToAuthenticate;
-use Laravel\Fortify\Actions\EnsureLoginNotThrottled;
+use Laravel\Fortify\Actions\EnsureLoginIsNotThrottled;
 use Laravel\Fortify\Actions\PrepareAuthenticatedSession;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Contracts\LoginResponse;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -17,10 +19,13 @@ class AuthenticatedSessionController extends Controller
 
     public function store(LoginRequest $request)
     {
-        return app(Pipeline::class)->send($request)->through([
-            EnsureLoginNotThrottled::class,
+        app(Pipeline::class)->send($request)->through([
+            EnsureLoginIsNotThrottled::class,
+            RedirectIfTwoFactorAuthenticatable::class,
             AttemptToAuthenticate::class,
             PrepareAuthenticatedSession::class,
         ])->thenReturn();
+
+        return app(LoginResponse::class);
     }
 }
