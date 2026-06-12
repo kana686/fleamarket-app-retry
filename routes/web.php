60a11939030decf2a54f965ea66a870588300a81
kana686 <inui.k.w6@gmail.com>
2026-06-12 18:03:00 +0900

    feat:画面・遷移確認用仮ルートを作成 #15

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,26 @@ Route::get('/home', function () {
     return '登録成功！';
 });
 
-Route::get('/', function () {
-    return 'ログイン成功！';
-});
+Route::get('/', function () { // 遷移確認用仮ルート
+    $items = [
+        (object) ['id' => 1, 'name' => '商品名1', 'img_url' => 'sample1.jpg'],
+        (object) ['id' => 2, 'name' => '商品名2', 'img_url' => 'sample2.jpg'],
+    ];
+
+    return view('items.index', compact('items'));
+})->name('items.index');
+
+Route::get('/items/{item}', function ($item) { // 遷移確認用仮ルート
+    return '商品詳細画面へ遷移成功！ (受け取ったID: '.$item.')';
+})->name('items.show');
+
+Route::get('/mypage', function () { // 遷移確認用仮ルート
+    return 'マイページ画面へ遷移確認用';
+})->name('mypage');
+
+Route::get('/sell', function () { // 遷移確認用仮ルート
+    return '出品画面への遷移確認';
+})->name('sell');
 
 Route::get('/register', [RegisteredUserController::class, 'create'])->name('register.create');
 Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
