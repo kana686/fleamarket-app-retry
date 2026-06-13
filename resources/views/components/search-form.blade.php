789a208da4e2347723c9de2aab9eafc13060c10f
kana686 <inui.k.w6@gmail.com>
2026-06-13 23:46:36 +0900

    refactor: 検索フォームの構造変更とスタイル調整 #15

--- resources/views/components/search-form.blade.php
+++ resources/views/components/search-form.blade.php
@@ -1,11 +1,12 @@
 @props(['action'])
-
-<form action="{{ $action }}" method="GET">
-    <input 
-        type="text" 
-        name="keyword" 
-        placeholder="なにをお探しですか？" 
-        value="{{ request()->query('keyword') }}"
-        class="search-input"
-    >
-</form>
+<div class="search-wrapper">
+    <form action="{{ $action }}" method="GET">
+        <input 
+            type="text" 
+            name="keyword" 
+            placeholder="なにをお探しですか？" 
+            value="{{ request()->query('keyword') }}"
+            class="search-input"
+        >
+    </form>
+</div>
