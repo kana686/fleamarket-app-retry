5ae2be57c4bc9dc37db32009b315d07f89be3c1b
kana686 <inui.k.w6@gmail.com>
2026-06-13 22:27:47 +0900

    refactor:スタイルを調整しやすくするため、タブ部分を切り分け #15

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout title="商品一覧">
-    <div class="items-container">
+    <div class="tabs-wrapper">
         <div class="tabs">
             <x-tab name="recommend" routeName="items.index">おすすめ</x-tab>
             <x-tab name="mylist" routeName="items.index">マイリスト</x-tab>
         </div>
+    </div>
 
+    <div class="items-container">
         <div class="item-grid">
             @forelse ($items as $item)
                 <x-item-card :item="$item" />
