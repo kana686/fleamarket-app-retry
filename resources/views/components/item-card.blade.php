120661c5588e10d90569ebfd6ba0038f2257399b
kana686 <inui.k.w6@gmail.com>
2026-06-14 15:43:51 +0900

    style:スタイルの調整とそれに伴うコンポーネントの修正 #15

--- resources/views/components/item-card.blade.php
+++ resources/views/components/item-card.blade.php
@@ -1,8 +1,12 @@
 @props(['item'])
 
-<div class="item-card">
+<div class="item-card" data-text="{{ $item->name }}">
     <a href="{{ route('items.show', $item->id) }}">
-        <img src="{{ asset('storage/' . $item->img_url) }}" alt="{{ $item->name }}">
-        <div class="item-name">{{ $item->name }}</div>
+        <div class="card-image-wrap">
+            <img src="{{ asset('storage/' . $item->img_url) }}" alt="{{ $item->name }}">
+        </div>
+        <div class="card-body">
+            <h3 class="item-name" >{{ $item->name }}</h3>
+        </div>
     </a>
 </div>
