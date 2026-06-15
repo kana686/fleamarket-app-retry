82b4c7f3455ec3f0538d9d4d0cefa9aa1e67f6b6
kana686 <inui.k.w6@gmail.com>
2026-06-15 17:44:26 +0900

    refactor:/の仮ルートを修正 #16

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
     return '登録成功！';
 });
-
-Route::get('/', function () { // 遷移確認用仮ルート
-    $items = [
-        (object) ['id' => 1, 'name' => '商品名1', 'img_url' => 'sample1.jpg'],
-        (object) ['id' => 2, 'name' => '商品名2', 'img_url' => 'sample2.jpg'],
-    ];
-
-    return view('items.index', compact('items'));
-})->name('items.index');
 
 Route::get('/items/{item}', function ($item) { // 遷移確認用仮ルート
     return '商品詳細画面へ遷移成功！ (受け取ったID: '.$item.')';
@@ -38,3 +30,5 @@ Route::controller(AuthenticatedSessionController::class)->group(function () {
     Route::get('/login', 'create')->name('login.create');
     Route::post('/login', 'store')->name('login.store');
 });
+
+Route::get('/', [ItemController::class, 'index'])->name('items.index');
