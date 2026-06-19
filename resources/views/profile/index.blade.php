19c6da4ae68fed3f44a3150a48e552fe4df9cecd
kana686 <inui.k.w6@gmail.com>
2026-06-19 16:31:46 +0900

    refactor:プロフィール画面でもタブの切り替え(選択時)のスタイルが適用されるように修正 #23

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,20 +1,17 @@
 <x-app-layout title="プロフィール">
-    <div class="profile-image-section">
+    <div class="profile-section">
         <x-img-field :src="$user->img_url" />
+
+        <div class="profile-info">
+            <h1 class="user-name">{{ $user->name }}</h1>
+            <a href="{{ route('profile.edit') }}" class="btn-edit">プロフィールを編集</a>
+        </div>
     </div>
-
-    <h1 class="user-name">
-            {{ $user->name }}
-    </h1>
-
-    <a href="{{ route('profile.edit') }}" class="btn-edit">
-            プロフィールを編集
-    </a>
 
     <div class="tabs-wrapper">
         <div class="tabs">
-            <x-tab name="sell" routeName="items.index">出品した商品</x-tab>
-            <x-tab name="buy" routeName="items.index">購入した商品</x-tab>
+            <x-tab name="sell" routeName="mypage" defaultName="sell">出品した商品</x-tab>
+            <x-tab name="buy" routeName="mypage" defaultName="sell">購入した商品</x-tab>
         </div>
     </div>
 
