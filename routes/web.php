d4631dbe01ea3ff34dd764cabcd3631a6a80ec85
kana686 <inui.k.w6@gmail.com>
2026-06-19 15:16:16 +0900

    refactor:/mypageの遷移確認用ルートを正式なものに修正 #23

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ Route::get('/items/{item}', function ($item) { // 遷移確認用仮ルート
     return '商品詳細画面へ遷移成功！ (受け取ったID: '.$item.')';
 })->name('items.show');
 
-Route::get('/mypage', function () { // 遷移確認用仮ルート
-    return 'マイページ画面へ遷移確認用';
-})->name('mypage');
-
 Route::get('/sell', function () { // 遷移確認用仮ルート
     return '出品画面への遷移確認';
 })->name('sell');
@@ -32,6 +28,7 @@ Route::get('/', [ItemController::class, 'index'])->name('items.index');
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('mypage')->controller(ProfileController::class)->group(function () {
+        Route::get('/', 'index')->name('mypage');
         Route::get('/profile', 'edit')->name('profile.edit');
         Route::patch('/profile', 'update')->name('profile.update');
     });
