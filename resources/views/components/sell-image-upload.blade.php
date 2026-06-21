6fffb2296fa16b6e2cb6abacf8166644b29eb2ee
kana686 <inui.k.w6@gmail.com>
2026-06-21 16:08:18 +0900

    feat:コンポーネントを作成 #27

---
+++ resources/views/components/sell-image-upload.blade.php
@@ -0,0 +1,7 @@
+<div class="sell-image-container">
+    <p class="section-label">商品画像</p>
+    <label class="sell-image-upload-area">
+        <input type="file" name="image" class="hidden-input" accept="image/*">
+        <x-file-upload-button />
+    </label>
+</div>
