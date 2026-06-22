0c341f57967ca433a826595abb2ebdc96a9041d0
kana686 <inui.k.w6@gmail.com>
2026-06-22 22:56:29 +0900

    fix:画像アップロードエリアの点線が表示されなくなったため修正 #27

--- resources/views/components/sell-image-upload.blade.php
+++ resources/views/components/sell-image-upload.blade.php
@@ -1,7 +1,8 @@
 <div class="sell-image-container">
     <p class="section-label">商品画像</p>
 
-    <img id="sell-image-preview" class="preview-image" src="#" alt="プレビュー" style="display:none;">
-
-    <x-file-upload-button name="img_url" id="sell-image-input" />
+    <label class="sell-image-upload-area">
+        <img id="sell-image-preview" class="preview-image" src="#" alt="プレビュー" style="display:none;">
+        <x-file-upload-button name="img_url" id="sell-image-input" />
+    </label>
 </div>
