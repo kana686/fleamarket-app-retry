5991a8ad81f758d5762ad46bc8198f5d7d06cafe
kana686 <inui.k.w6@gmail.com>
2026-06-22 22:40:17 +0900

    feat:選択した画像のプレビュー機能を実装 #27

--- resources/views/components/sell-image-upload.blade.php
+++ resources/views/components/sell-image-upload.blade.php
@@ -1,7 +1,7 @@
 <div class="sell-image-container">
     <p class="section-label">商品画像</p>
-    <label class="sell-image-upload-area">
-        <input type="file" name="image" class="hidden-input" accept="image/*">
-        <x-file-upload-button />
-    </label>
+
+    <img id="sell-image-preview" class="preview-image" src="#" alt="プレビュー" style="display:none;">
+
+    <x-file-upload-button name="img_url" id="sell-image-input" />
 </div>
