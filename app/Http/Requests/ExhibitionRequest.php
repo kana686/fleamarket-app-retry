f57ec679be0a4be0cdd93c206c33336aa8a18d6c
kana686 <inui.k.w6@gmail.com>
2026-06-23 00:43:19 +0900

    feat:ExhibitionRequestを作成し、バリデーションルールとエラーメッセージを設定 #28

---
+++ app/Http/Requests/ExhibitionRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExhibitionRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'brand_name' => ['nullable', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:255'],
+            'img_url' => ['required', 'image', 'mimes:jpeg,png'],
+            'categories'   => ['required', 'array'],
+            'categories.*' => ['integer', 'exists:categories,id'],
+            'condition_id' => ['required', 'integer', 'exists:conditions,id'],
+            'price' => ['required', 'integer', 'min:0'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '商品名を入力してください',
+            'name.max' => '商品名は255文字以内で入力してください',
+            'brand_name.max' => 'ブランド名は255文字以内で入力してください',
+            'description.required' => '商品の説明を入力してください',
+            'description.max'=> '商品の説明は255文字以内で入力してください',
+            'img_url.required' => '画像を選択してください',
+            'img_url.image' => '画像ファイルをアップロードしてください',
+            'img_url.mimes' => '画像はjpegまたはpng形式でアップロードしてください',
+            'categories.required' => 'カテゴリーを1つ以上選択してください',
+            'condition_id.required' => '商品の状態を選択してください',
+            'price.required' => '販売価格を入力してください',
+            'price.integer' => '販売価格は数値で入力してください',
+            'price.min' => '販売価格は0円以上で入力してください',
+        ];
+    }
+
+}
