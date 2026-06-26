6bb6281493838ef8eb825bc55dd50f3e6c41282b
kana686 <inui.k.w6@gmail.com>
2026-06-26 17:44:31 +0900

    feat:LikeServiceにstore(),destroy()のロジックを記述 #32

---
+++ app/Services/LikeService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Mylist;
+
+class LikeService
+{
+    public function addLike($itemId, $userId)
+    {
+        return Mylist::create(['item_id' => $itemId, 'user_id' => $userId]);
+    }
+
+    public function removeLike($itemId, $userId)
+    {
+        return Mylist::where('item_id', $itemId)->where('user_id', $userId)->delete();
+    }
+}
