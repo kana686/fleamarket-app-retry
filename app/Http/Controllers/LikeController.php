58143d3e33799ad2391c2b37a2feaf99167e02cd
kana686 <inui.k.w6@gmail.com>
2026-06-26 17:55:18 +0900

    refactor:LikeServiceをインポートできていなかったため修正 #32

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,8 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\LikeService;
+
 class LikeController extends Controller
 {
+    protected $likeService;
+
+    public function __construct(LikeService $likeService)
+    {
+        $this->likeService = $likeService;
+    }
+
     public function store($id)
     {
         $this->likeService->addLike($id, auth()->id());
