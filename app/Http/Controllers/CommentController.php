9fdcbeaec4e2ff59c900271e44b6a16c6c0c22a0
kana686 <inui.k.w6@gmail.com>
2026-06-28 00:09:39 +0900

    feat:コントローラーを作成 #32

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CommentRequest;
+use App\Services\CommentService;
+
+class CommentController extends Controller
+{
+    public function store(CommentRequest $request, Item $item, CommentService $service)
+    {
+        $service->createComment($request->validated(), $item->id, auth()->id());
+
+        return back()->with('success', 'コメントを投稿しました。');
+    }
+}
