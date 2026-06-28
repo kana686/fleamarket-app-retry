4e30b3ec2678709ddf731375b90c26bf1c53168e
kana686 <inui.k.w6@gmail.com>
2026-06-29 01:40:21 +0900

    refactor:セッションに保存した住所を表示できるように修正 #35

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -9,11 +9,14 @@ class PurchaseController extends Controller
         $item = Item::findOrFail($item_id);
         $user = Auth::user();
 
+        $shippingAddress = session('edited_address', $user->address);
+
         $paymentMethods = [
             1 => 'コンビニ払い',
             2 => 'クレジット払い',
         ];
+        $paymentMethod = session('selected_payment_method', 1);
 
-        return view('purchases.checkout', compact('item', 'user', 'paymentMethods'));
+        return view('purchases.checkout', compact('item', 'user', 'shippingAddress', 'paymentMethods'));
     }
 }
