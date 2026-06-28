ed575ee798cd5033ee08e98ef8175b23ab9e6f9d
kana686 <inui.k.w6@gmail.com>
2026-06-29 00:41:42 +0900

    refactor:連想配列も受け取れるように修正 #35

--- resources/views/components/select-field.blade.php
+++ resources/views/components/select-field.blade.php
@@ -4,17 +4,17 @@
     <label for="{{ $name }}">{{ $label }}</label>
     <select name="{{ $name }}" id="{{ $name }}" class="form-select">
         <option value="" hidden selected>選択してください</option>
-        
-        @foreach($options as $option)
-            <option 
-                value="{{ $option->id }}" 
+
+        @foreach($options as $id => $labelName)
+            <option
+                value="{{ $id }}"
                 {{ (old($name, $selected) == $option->id) ? 'selected' : '' }}
             >
-                {{ $option->content }}
+                {{ $labelName }}
             </option>
         @endforeach
     </select>
-    
+
     @error($name)
         <span class="error-message">{{ $message }}</span>
     @enderror
