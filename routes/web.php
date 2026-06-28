eda3651af4fc7c0b2d290d375807a849ee82eced
kana686 <inui.k.w6@gmail.com>
2026-06-28 23:09:31 +0900

    feat:ルートを設定 #35

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,9 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/purchase/{item}', function ($item) { // 遷移確認用仮ルート
-    return '商品購入画面へ遷移成功！ (受け取ったID: '.$item.')';
-})->name('purchases.checkout');
 
 Route::controller(RegisteredUserController::class)->group(function () {
     Route::get('/register', 'create')->name('register.create');
@@ -42,6 +39,11 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('items')->controller(LikeController::class)->group(function () {
         Route::post('/{id}/like', 'store')->name('like.store');
         Route::delete('/{id}/like', 'destroy')->name('like.destroy');
+    });
+
+    Route::prefix('purchase')->controller(PurchaseController::class)->group(function () {
+        Route::get('/{item}', 'show')->name('purchases.checkout');
+        Route::get('/address/{item}', 'edit')->name('purchases.edit');
     });
 });
 
