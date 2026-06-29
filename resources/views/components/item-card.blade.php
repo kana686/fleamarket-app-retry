f4dae5704da1787639598a99de32f7074cfa29ca
kana686 <inui.k.w6@gmail.com>
2026-06-30 01:52:17 +0900

    refactor:購入された商品にsoldと表示されるように設定 #36

--- resources/views/components/item-card.blade.php
+++ resources/views/components/item-card.blade.php
@@ -4,6 +4,10 @@
     <a href="{{ route('items.show', $item->id) }}">
         <div class="card-image-wrap">
             <img src="{{ asset('storage/' . $item->img_url) }}" alt="{{ $item->name }}">
+
+            @if ($item->purchases()->exists())
+                <span class="sold-ribbon">Sold</span>
+            @endif
         </div>
         <div class="card-body">
             <h3 class="item-name" >{{ $item->name }}</h3>
