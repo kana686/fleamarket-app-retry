cb567a36b43a395044470bc2bf7061346206e0bb
kana686 <inui.k.w6@gmail.com>
2026-06-29 22:26:20 +0900

    refactor:payment_methodを数値で管理するため、モデルに定数として定義 #36

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -9,13 +9,26 @@ class Purchase extends Model
 {
     use HasFactory;
 
+    const PAYMENT_METHOD_CONVENIENCE = 1;
+
+    const PAYMENT_METHOD_CREDIT = 2;
+
     protected $fillable = [
         'item_id',
         'user_id',
         'post_code',
         'address',
         'building',
+        'payment_method',
     ];
+
+    public static function getPaymentMethodLabels(): array
+    {
+        return [
+            self::PAYMENT_METHOD_CONVENIENCE => 'コンビニ払い',
+            self::PAYMENT_METHOD_CREDIT => 'クレジット払い',
+        ];
+    }
 
     public function item()
     {
