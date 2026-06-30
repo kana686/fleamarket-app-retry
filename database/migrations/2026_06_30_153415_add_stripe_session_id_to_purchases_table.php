7134130e53d7f2ebf39e83bae257aa95fc6ca5d4
kana686 <inui.k.w6@gmail.com>
2026-06-30 15:39:08 +0900

    feat:stripeの決済IDとステータスを保存するカラムを追加 #36

---
+++ database/migrations/2026_06_30_153415_add_stripe_session_id_to_purchases_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('purchases', function (Blueprint $table) {
+            Schema::table('purchases', function (Blueprint $table) {
+                $table->string('stripe_session_id')->nullable()->after('address');
+                $table->string('status')->default('pending')->after('stripe_session_id');
+            });
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('purchases', function (Blueprint $table) {
+            Schema::table('purchases', function (Blueprint $table) {
+                $table->dropColumn(['stripe_session_id', 'status']);
+            });
+        });
+    }
+};
