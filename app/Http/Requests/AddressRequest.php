649fff64be73623771bbc1c0566596c6b0ce9206
kana686 <inui.k.w6@gmail.com>
2026-07-01 01:20:00 +0900

    feat:TrimsInputを追加 #38

--- app/Http/Requests/AddressRequest.php
+++ app/Http/Requests/AddressRequest.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\TrimsInput;
 use Illuminate\Foundation\Http\FormRequest;
 
 class AddressRequest extends FormRequest
 {
+    use TrimsInput;
+
     public function authorize(): bool
     {
         return true;
@@ -26,6 +29,8 @@ class AddressRequest extends FormRequest
             'post_code.required' => '郵便番号を入力してください。',
             'post_code.regex' => '郵便番号は「000-0000」の形式で入力してください。',
             'address.required' => '住所を入力してください。',
+            'address.max' => '住所は255文字以内で入力してください。',
+            'building.max' => '建物名は255文字以内で入力してください。',
         ];
     }
 }
