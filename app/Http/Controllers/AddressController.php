602c407dcb00433c3ce4d93111c44008ddaae0c9
kana686 <inui.k.w6@gmail.com>
2026-07-01 01:07:25 +0900

    feat:AddressServiceを作成してロジックを委譲 #38

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -2,25 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddressRequest;
 use App\Models\Item;
+use App\Services\AddressService;
 
 class AddressController extends Controller
 {
+    protected $addressService;
+
+    public function __construct(AddressService $addressService)
+    {
+        $this->addressService = $addressService;
+    }
+
     public function edit(Item $item)
     {
-        $itemId = $item->id;
-
-        $address = (object) session("temp_address.{$itemId}", [
-            'post_code' => auth()->user()->post_code,
-            'address' => auth()->user()->address,
-            'building' => auth()->user()->building,
-        ]);
+        $address = (object) $this->addressService->getAddressData(auth()->user());
 
         return view('purchases.address', compact('item', 'address'));
     }
 
-    public function update(Request $request, Item $item)
+    public function update(AddressRequest $request, Item $item)
     {
+        $this->addressService->saveTemporaryAddress($request->validated());
+
         return redirect()->route('purchases.checkout', $item->id);
     }
 }
