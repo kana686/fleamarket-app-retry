5d317062f8c3cd47559406d428fb921fad2cdb77
kana686 <inui.k.w6@gmail.com>
2026-07-02 15:01:26 +0900

    feat:UserFactoryを作成 #47

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,40 +2,24 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends Factory<User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => 'password',
+            'post_code' => $this->faker->postcode(),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->secondaryAddress(),
+            'img_url' => $this->faker->imageUrl(),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
