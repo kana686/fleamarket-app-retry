1ad41f03d5462b8e90f5eff6a532633b9dbb5212
kana686 <inui.k.w6@gmail.com>
2026-07-02 22:57:08 +0900

    refactor:Pintによる整形 #49

--- database/seeders/MasterDataSeeder.php
+++ database/seeders/MasterDataSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\Category;
 use App\Models\Condition;
+use Illuminate\Database\Seeder;
 
 class MasterDataSeeder extends Seeder
 {
